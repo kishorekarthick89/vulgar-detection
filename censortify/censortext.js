5eece9623146b9b7d421eba70f2b2e066460fd9a
kishorekarthick89 <kishorekarthick89@gmail.com>
2025-03-29 13:36:31 +0530

    Add files via upload

--- censortify/censortext.js
+++ censortify/censortext.js
@@ -1,1 +1,20 @@
-
+var censoredWords = ["fuck", "bitch", "shit"];
+var customCensoredWords = []; 
+function censoredWords(inStr) {
+  for (idx in censoredWords) {
+    inStr = inStr.replace(censoredWords[idx], "****") 
+  }
+  for (idx in censoredWords) {
+    inStr = inStr.replace(customCensoredWords[idx], "****") 
+  }
+  return inStr;
+}
+function addCensoredWord(word) {
+  customCensoredWords.push(word);
+}
+function getCensoredWords() {
+  return censoredWords.push(word); 
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords; 
